d5c9ad087e0c1feff5a0256d1bf0cb3bc92c1e64
Ward Werbrouck <34573943+kletse@users.noreply.github.com>
2022-10-26 14:48:38 +0200

    cleanup

--- services/i18n/formatters/entity-formatters/textFormatter.ts
+++ services/i18n/formatters/entity-formatters/textFormatter.ts
@@ -1,8 +1,3 @@
-import { CartItem } from '@providers/cart/cartContext';
-import { BaseCartItem } from '@providers/cart/cartModels';
-import { defaultLanguage } from '@services/i18n';
-import { OrderLine } from '@services/portal-api';
-
 export class TextFormatter {
   public formatCamelCase(text: string): string {
     return text
@@ -24,15 +19,3 @@ export class TextFormatter {
     return text.substring(0, maxCharacters - 1).concat(trailingSuffix);
   }
 }
-//TODO ward: move to formatter
-export const formatCreditCardNumber = (cardNumber: string): string => {
-  return `${cardNumber
-    .substring(0, cardNumber.length - 4)
-    .replace(/[0-9]/g, '●')
-    .replace(/X/g, '●')}${cardNumber.substring(cardNumber.length - 4)}`;
-};
-
-//TODO ward: move to formatter
-export const formatCreditCardIssuer = (issuer: string): string => {
-  return issuer.replace(/^\w/, c => c.toUpperCase());
-};
