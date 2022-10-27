483cf0344a861beac025d4dac5df8ecd4a900dc0
Ward Werbrouck <34573943+kletse@users.noreply.github.com>
2022-10-27 10:13:33 +0200

    cleanup

--- widgets/checkout/checkout-form-groups/creditCardPaymentFormGroup.tsx
+++ widgets/checkout/checkout-form-groups/creditCardPaymentFormGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import valid from 'card-validator';
 import { FormikContextType, useFormikContext } from 'formik';
