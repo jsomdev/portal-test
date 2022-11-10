2f3eea6bbd6c222c42031b421912a5ba14a38407
Ward Werbrouck <34573943+kletse@users.noreply.github.com>
2022-11-10 13:49:27 +0100

    cleanup

--- widgets/checkout-new/steps/step-1-details/shippingContactFormGroup.tsx
+++ widgets/checkout-new/steps/step-1-details/shippingContactFormGroup.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import { FormikTextField } from '@components/formik-wrappers/formikTextField';
 import { Stack, useTheme } from '@fluentui/react';
 import { StepFields } from '@widgets/checkout-new/checkout.types';
-import { Step1 } from '@widgets/checkout-new/steps/step-1-details/step-1-details';
+import { Step1FormData } from '@widgets/checkout-new/steps/step-1-details/step-1-details';
 import { CheckoutFormGroupTitle } from '@widgets/checkout/shared/checkoutFormGroupTitle';
 import { CheckoutFormRowContainer } from '@widgets/checkout/shared/checkoutFormRowContainer';
 
 export const ShippingContactFormGroup: React.FC<{
   title?: string;
-  fields: Pick<StepFields<Step1>, 'firstName' | 'name' | 'company' | 'phone'>;
+  fields: Pick<
+    StepFields<Step1FormData>,
+    'firstName' | 'name' | 'company' | 'phone'
+  >;
 }> = ({ title, fields }) => {
   const messages = {
     shippingContact: title || 'Shipping Contact'
