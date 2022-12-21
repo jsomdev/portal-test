74862ccaed70da99a83bc71fa87d662f440f9fcb
Ward Werbrouck <34573943+kletse@users.noreply.github.com>
2022-12-21 16:38:26 +0100

    fix added to cart dialog card

--- widgets/cart-item-added-dialog/cartItemAddedCard.tsx
+++ widgets/cart-item-added-dialog/cartItemAddedCard.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
 
-import {
-  IStackItemStyles,
-  IStackStyles,
-  Stack,
-  useTheme
-} from '@fluentui/react';
 import { useMe } from '@providers/user/userContext';
-import { rem } from '@utilities/rem';
 import { CartItemCard } from '@widgets/cart-list/cartItemCard';
 import { CartItemViewModel } from '@widgets/cart-list/cartList.types';
-import { Mobile, TabletAndDesktop } from '@widgets/media-queries';
-import ProductCard from '@widgets/product-card-parts/productCard';
-import ProductCardImage from '@widgets/product-card-parts/productCardImage';
-import ProductCardTitleLink from '@widgets/product-card-parts/productCardTitleLink';
-
-type CartItemAddedCardStyles = {
-  leftContainer: IStackStyles;
-  mobilePrice: IStackItemStyles;
-};
 
 type CartItemAddedCardProps = {
   item: CartItemViewModel;
