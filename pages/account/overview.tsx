8d2daf414507359cb0436a2ea867dab7d3b77043
Jan <jan.somers@spray.com>
2023-01-24 14:04:35 +0100

    Missed a page

--- pages/account/overview.tsx
+++ pages/account/overview.tsx
@@ -14,6 +14,7 @@ import {
   fetchMenuItemsForMainHeader,
   fetchMenuItemsForSiteHeader
 } from '@services/portal-api/menuItems';
+import { ENVIRONMENT_VARIABLES } from '@utilities/environmentVariables';
 import { AccountPage } from '@widgets/account/accountPage';
 import { Overview } from '@widgets/account/overview/overview';
 import { getLocalePaths } from '@widgets/page/page.helper';
@@ -48,18 +49,6 @@ const Account: NextPage<
   );
 };
 
-export const getStaticPaths: GetStaticPaths =
-  async (): Promise<GetStaticPathsResult> => {
-    const localizedPaths = (supportedLocales || []).map(locale => {
-      return {
-        locale,
-        params: {}
-      };
-    });
-
-    return { paths: localizedPaths, fallback: 'blocking' };
-  };
-
 export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext
 ): Promise<
@@ -67,7 +56,11 @@ export const getStaticProps: GetStaticProps = async (
     Partial<Pick<GlobalDataContextProps, 'mainMenuItems' | 'siteMenuItems'>>
   >
 > => {
-  const { locale } = context;
+  const { locale: contextLocale } = context;
+  let locale = contextLocale;
+  if (locale === ENVIRONMENT_VARIABLES.defaultLocale || locale === 'default') {
+    locale = 'en-us';
+  }
   const [siteMenuData, mainMenuData] = await Promise.all([
     fetchMenuItemsForSiteHeader(locale),
     fetchMenuItemsForMainHeader(locale)
