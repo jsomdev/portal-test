a8b2733623e6ea7b0b7adae2087930e90aa8e4b1
Ward Werbrouck <Ward.Werbrouck@spray.com>
2023-01-25 09:18:43 +0000

    Merged PR 13809: middleware debugging
    middleware debugging

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,18 @@ const PUBLIC_FILE = /\.(.*)$/;
 export async function middleware(
   req: NextRequest
 ): Promise<NextResponse | undefined> {
+  console.error('middleware log', JSON.stringify(req.nextUrl.pathname));
   if (
     req.nextUrl.pathname.startsWith('/_next') ||
     req.nextUrl.pathname.includes('/api/') ||
     PUBLIC_FILE.test(req.nextUrl.pathname)
   ) {
+    console.error('middleware IF 1');
     return;
   }
 
   if (req.nextUrl.locale === 'default') {
+    console.error('middleware IF 2');
     const locale = 'en-us';
 
     return NextResponse.redirect(
